82dec3ba37177083b87a27c4971ae9856de78175
Ram Kumar Rengaswamy <ramkumarr@gmail.com>
2022-05-06 15:47:58 -0400

    Rename to wasm-record-printer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use wasmflow::{flow::FlowProcessor, sources::kafka::KafkaStream};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    let fctx = FlowProcessor::new("./target/wasm32-wasi/release/wasm_record_processor.wasm");
+    let fctx = FlowProcessor::new("./target/wasm32-wasi/release/wasm_record_printer.wasm");
     let stream = KafkaStream::new("ramr_test_topic", fctx).await?;
     stream.start().await?;
     Ok(())

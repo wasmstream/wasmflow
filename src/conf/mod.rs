fd11a600330a8c76f8081319e8ad96cc0dee78cd
Ram Kumar Rengaswamy <ramkumarr@gmail.com>
2022-08-02 21:49:34 +0000

    Add tests for conf

--- src/conf/mod.rs
+++ src/conf/mod.rs
@@ -59,10 +59,64 @@ pub struct FlowConfig {
 pub fn read_config() -> Result<FlowConfig> {
     let fname = std::env::var("WASMFLOW_CONFIG")
         .with_context(|| "Error reading WASMFLOW_CONFIG var. Did you remember to set it?")?;
-    let yaml_str = fs::read_to_string(fname.clone())
-        .with_context(|| format!("Error reading conf file {fname}"))?;
+    read_config_file(&fname)
+}
+
+pub fn read_config_file(fname: &str) -> Result<FlowConfig> {
+    let yaml_str =
+        fs::read_to_string(fname).with_context(|| format!("Error reading conf file {fname}"))?;
     let conf: FlowConfig = serde_yaml::from_str(&yaml_str)
         .with_context(|| format!("Error parsing YAML conf file {fname}"))?;
     info!(conf=?conf);
     Ok(conf)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_read_config() {
+        let cfg = read_config_file("./src/conf/wasmflow.yml").unwrap();
+        assert_eq!(cfg.sources.len(), 1);
+        match &cfg.sources[0] {
+            Source::Kafka {
+                brokers,
+                group_id,
+                topic,
+                batch_size,
+                sasl,
+            } => {
+                assert_eq!(brokers.len(), 1);
+                assert_eq!(brokers[0], "my-broker.confluent.cloud:9092");
+                assert_eq!(group_id, "wasmflow-group");
+                assert_eq!(topic, "my-topic");
+                assert_eq!(*batch_size, 1000000);
+                assert!(matches!(
+                    sasl,
+                    SaslConfig::Plain {
+                        username: _,
+                        password: _
+                    }
+                ));
+            }
+        }
+        assert_eq!(cfg.sinks.len(), 1);
+        match &cfg.sinks[0] {
+            Sink::S3 {
+                region,
+                bucket,
+                key_prefix,
+                file_size,
+            } => {
+                assert_eq!(region, "us-east-1");
+                assert_eq!(bucket, "wasmtime-sink");
+                assert_eq!(key_prefix, "my-stream");
+                assert_eq!(*file_size, 4096);
+            }
+            _ => {
+                panic!("Incorrect sink config");
+            }
+        }
+        assert_eq!(cfg.processors.len(), 1);
+    }
 }
